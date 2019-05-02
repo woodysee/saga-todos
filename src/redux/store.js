80c703a8d0d7a6853a099520a7fd9a17d5fe1ed7
Woody See <woody.see@lazada.com>
2019-05-02 15:20:35 +0800

    Implement saga middleware to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import todoApp from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   todoApp,
   window.STATE_FROM_SERVER /* Optional preloaded state */,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  /* Show store on redux dev tools. TODO: Hide in production. */
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    /* Show store on redux dev tools. TODO: Hide in production. */
+  )
 );
 export { store as reduxStore };
